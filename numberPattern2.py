e38e6c677fa5b7d26692974737ab9ef43a76ece7
anikamurarka <51739084+anikamurarka@users.noreply.github.com>
2022-01-10 21:34:53 +0530

    Create numberPattern2.py

---
+++ numberPattern2.py
@@ -0,0 +1,20 @@
+# 1
+# 11
+# 202
+# 3003
+
+n =int(input())
+i =  1
+if n == 1:
+	print (1)
+else :
+	print(1)
+	i=1
+	while i<n:
+		print(i,end='')
+		j = 2
+		while j<=i:
+			print(0, end='')
+			j = j+1
+		print(i)
+		i=i+1
