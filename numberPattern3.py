d298f930deb255639afd8a20f1f6039540b4e7f9
anikamurarka <51739084+anikamurarka@users.noreply.github.com>
2022-01-10 21:35:34 +0530

    Create numberPattern3.py

---
+++ numberPattern3.py
@@ -0,0 +1,16 @@
+# 1
+# 11
+# 121
+# 1221
+
+n =int(input())
+i =  1
+print (1)
+while i<n:
+    print(1, end='')
+    j = 2
+    while j<=i:
+        print(2, end='')
+        j = j+1
+    print(1)
+    i=i+1
